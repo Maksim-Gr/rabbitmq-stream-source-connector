ca6c5df082afee30a3ec83903eafab719b35e7df
Maksim Grishchenko <grischenko1988@gmail.com>
2025-05-17 20:32:41 +0300

    chore: create README.md && provide build script

---
+++ README.md
@@ -0,0 +1,62 @@
+# RabbitMQ streams source connector
+RabbitMQ source connector for reading data from rabbitmq streams and write to kafka
+
+## Installation
+Follow these steps to install and deploy the RabbitMQ Source Connector:
+### 1. Build the Connector
+First, build the connector JAR file using Gradle:
+```bash
+  ./gradlew clean build
+```
+This command will clean the project and build the JAR file. The resulting JAR will be located in the `build/libs/` directory.
+
+---
+
+### 2. Copy the JAR to Kafka Connect Plugin Directory
+After building the JAR, copy it into your Kafka Connect plugin path. You can do so by running the following command:
+
+```bash
+  cp build/libs/rabbitmq-source-connector.jar $KAFKA_CONNECT_PLUGINS_DIR/
+```
+
+Make sure that `$KAFKA_CONNECT_PLUGINS_DIR/` points to the correct directory where Kafka Connect loads its connectors (this is specified in the Kafka Connect worker's `plugin.path` configuration).
+
+---
+### 3. Restart Kafka Connect
+
+# Configuration
+
+| Property                                   | Description                   |
+|--------------------------------------------|-------------------------------|
+| connector.class                             | Connector class to use, must be `com.altenar.RabbitSourceConnector` |
+| tasks.max                                   | Maximum number of tasks to run |
+| rabbitmq.host                               | Hostname of the RabbitMQ broker |
+| rabbitmq.port                               | Port of the RabbitMQ broker   |
+| rabbitmq.username                           | Username for connecting to RabbitMQ |
+| rabbitmq.password                           | Password for connecting to RabbitMQ |
+| rabbitmq.virtual.host                       | Virtual host on RabbitMQ to connect to |
+| rabbitmq.queue                              | Name of the RabbitMQ queue to consume from |
+| rabbitmq.handshake.timeout.ms               | Timeout in milliseconds for RabbitMQ handshake |
+| rabbitmq.offset                             | Initial offset to consume from, e.g., `first` |
+| kafka.topic                                 | Kafka topic where the data should be published |
+
+
+### Connector config
+```json
+{
+  "name": "RabbitSourceConnector",
+  "config": {
+    "connector.class": "com.altenar.RabbitSourceConnector",
+    "tasks.max": "1",
+    "rabbitmq.host": "localhost",
+    "rabbitmq.port": "5672",
+    "rabbitmq.username": "guest",
+    "rabbitmq.password": "guest",
+    "rabbitmq.virtual.host": "/",
+    "rabbitmq.queue": "queue_name_here",
+    "rabbitmq.handshake.timeout.ms": "10000",
+    "rabbitmq.offset": "first",
+    "kafka.topic": "your_kafka_topic",
+  }
+}
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+import java.time.LocalDateTime
+
+val currentDateTime: LocalDateTime = LocalDateTime.now()
+
 plugins {
-    kotlin("jvm") version "2.1.20"
+    `java-library`
+    kotlin("jvm") version "2.1.0"
+    id("com.palantir.git-version") version "1.0.0"
+    kotlin("plugin.serialization") version "1.5.0"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+    `maven-publish`
 }
 
 group = "com.github.maksimgr"
@@ -7,12 +16,96 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenCentral()
+    maven { url = uri("https://packages.confluent.io/maven/") }
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
+    implementation("org.apache.kafka:connect-api:3.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("commons-validator:commons-validator:1.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    testImplementation("io.confluent:kafka-connect-avro-converter:7.3.0")
+    testImplementation("com.github.christophschubert:cp-testcontainers:v0.2.1")
+    testImplementation("org.testcontainers:kafka:1.19.6")
+    testImplementation("org.sourcelab:kafka-connect-client:4.0.3")
+    testImplementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
+    testImplementation("org.mockito:mockito-core:3.12.4")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2") // JUnit Jupiter
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher") // JUnit Platform Launcher
+    implementation("com.rabbitmq:stream-client:0.16.0")
+    testImplementation("org.mockito:mockito-core:5.5.0")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.0")
+    testImplementation("org.testcontainers:rabbitmq:1.19.0")
+    testImplementation("com.rabbitmq:amqp-client:5.18.0")
     testImplementation(kotlin("test"))
+}
+ktlint {
+    version.set("1.2.1")
+    android.set(false)
+    outputColorName.set("RED")
+    verbose.set(true)
+    reporters {
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
+    reports.html.required.set(true)
+    testLogging {
+        events("passed", "skipped", "failed")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showStandardStreams = true
+    }
+}
+kotlin {
+    jvmToolchain(11)
+}
+tasks.jar {
+    val dependencies =
+        configurations
+            .runtimeClasspath
+            .get()
+            .map(::zipTree)
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    manifest {
+        attributes(
+            mapOf(
+                "Build-Timestamp" to currentDateTime,
+//                "Build-Revision" to details.gitHash,
+//                "Build-Is-Clean" to details.isCleanTag,
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version,
+            ),
+        )
+    }
+}
+
+tasks.register("generateVersion") {
+    description = "Create a version properties file in the build folder"
+    doLast {
+        sourceSets.main
+            .get()
+            .output.resourcesDir
+            ?.mkdirs()
+        file("${sourceSets.main.get().output.resourcesDir}/version.properties").writeText(
+            """
+            Build-Timestamp=$currentDateTime
+            
+            Connector-Version=${project.version},
+            "Implementation-Title" to ${project.name},
+            "Implementation-Version" to ${project.version},
+            """.trimIndent(),
+        )
+    }
+}
+tasks.build {
+    dependsOn(":generateVersion")
 }
