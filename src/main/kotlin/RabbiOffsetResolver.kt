cd7bd3d4946ddbb3442ad025fe44d77472e5ab2b
Maksim Grishchenko <grischenko1988@gmail.com>
2025-05-30 10:35:50 +0300

    chore(offsetResolver): add object offset resolver

---
+++ src/main/kotlin/RabbiOffsetResolver.kt
@@ -0,0 +1,32 @@
+import com.rabbitmq.stream.OffsetSpecification
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+
+object RabbiOffsetResolver {
+    private val timeStampPattern = Regex("""\d{2}\.\d{2}\.\d{4}\d{2}:\d{2}:\d{2}""")
+    private val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyyHH:mm:ss")
+
+    fun resolveOffset(offsetStr: String): OffsetSpecification {
+        return when (offsetStr.lowercase()) {
+            "first" -> OffsetSpecification.first()
+            "last" -> OffsetSpecification.last()
+            "next" -> OffsetSpecification.next()
+            else -> {
+                if (timeStampPattern.matches(offsetStr)) {
+                    try {
+                        val dateTime = LocalDateTime.parse(offsetStr, formatter)
+                        val timestamp = dateTime.toEpochSecond(ZoneOffset.UTC)
+                        OffsetSpecification.timestamp(timestamp)
+                    } catch (e: Exception) {
+                        OffsetSpecification.first()
+                    }
+                } else {
+                    OffsetSpecification.first()
+                }
+            }
+        }
+    }
+
+}
