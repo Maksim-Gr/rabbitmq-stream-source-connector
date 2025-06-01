44770b48c9cd1887d2c12b9c0fe92cc0a683beb7
Maksim Grishchenko <grischenko1988@gmail.com>
2025-06-01 20:54:21 +0300

    test(OffsetResolver): test object methods for offset resolver

---
+++ src/test/kotlin/RabbiOffsetResolverTest.kt
@@ -0,0 +1,42 @@
+import com.rabbitmq.stream.OffsetSpecification
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class RabbiOffsetResolverTest {
+    @Test
+    @DisplayName("Should return OffsetSpecification.first for first")
+    fun testFirstOffset() {
+        val result = RabbiOffsetResolver.resolveOffset("first")
+        assertEquals(result, OffsetSpecification.first())
+    }
+
+    @Test
+    @DisplayName("Should return OffsetSpecification.last for last")
+    fun testLastOffset() {
+        val result = RabbiOffsetResolver.resolveOffset("last")
+        assertEquals(result, OffsetSpecification.last())
+    }
+
+
+    @Test
+    @DisplayName("Should return OffsetSpecification.next for next")
+    fun testNextOffset() {
+        val result = RabbiOffsetResolver.resolveOffset("next")
+        assertEquals(result, OffsetSpecification.next())
+    }
+
+
+    @Test
+    @DisplayName("Should parse valid timestamp into OffsetSpecification.timestamp")
+    fun testValidTimestamp() {
+        val input = "2023-01-01 12:00:00"
+        val expectedTimestamp = LocalDateTime.parse(input, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+            .toEpochSecond(java.time.ZoneOffset.UTC)
+
+        val result = RabbiOffsetResolver.resolveOffset(input)
+        assertEquals(result, OffsetSpecification.timestamp(expectedTimestamp))
+    }
+}
