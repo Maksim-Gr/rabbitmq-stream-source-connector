d41783e7717c8b8060110b13ebbebde052b25206
Maksim Grishchenko <grischenko1988@gmail.com>
2026-03-04 21:29:20 +0300

    chore: remove redundant  regex

--- src/main/kotlin/RabbiOffsetResolver.kt
+++ src/main/kotlin/RabbiOffsetResolver.kt
@@ -4,27 +4,23 @@ import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 object RabbiOffsetResolver {
-    private val timeStampPattern = Regex("""\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}:\d{2}""")
     private val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")
 
-    fun resolveOffset(offsetStr: String): OffsetSpecification {
-        return when (offsetStr.lowercase()) {
+    fun resolveOffset(offsetStr: String): OffsetSpecification =
+        when (offsetStr.lowercase()) {
             "first" -> OffsetSpecification.first()
             "last" -> OffsetSpecification.last()
             "next" -> OffsetSpecification.next()
-            else -> {
-                if (timeStampPattern.matches(offsetStr)) {
-                    try {
-                        val dateTime = LocalDateTime.parse(offsetStr, formatter)
-                        val timestamp = dateTime.toEpochSecond(ZoneOffset.UTC)
-                        OffsetSpecification.timestamp(timestamp)
-                    } catch (e: Exception) {
-                        OffsetSpecification.first()
-                    }
-                } else {
-                    OffsetSpecification.first()
-                }
-            }
+            else ->
+                offsetStr.toTimestampOrNull()?.let {
+                    OffsetSpecification.timestamp(it)
+                } ?: OffsetSpecification.first()
         }
-    }
+
+    private fun String.toTimestampOrNull(): Long? =
+        runCatching {
+            LocalDateTime
+                .parse(this, formatter)
+                .toEpochSecond(ZoneOffset.UTC)
+        }.getOrNull()
 }
