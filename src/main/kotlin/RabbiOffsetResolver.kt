c2654b3dcb378fdbed0dfe5c23c5ed49f815e362
Maksim Grishchenko <grischenko1988@gmail.com>
2026-03-05 22:06:22 +0300

    chore: change behaviour to exclude silent fail

--- src/main/kotlin/RabbiOffsetResolver.kt
+++ src/main/kotlin/RabbiOffsetResolver.kt
@@ -1,20 +1,28 @@
 import com.rabbitmq.stream.OffsetSpecification
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
 object RabbiOffsetResolver {
+    val logger: Logger = LoggerFactory.getLogger(RabbitSourceConnector::class.java)
     private val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")
 
     fun resolveOffset(offsetStr: String): OffsetSpecification =
-        when (offsetStr.lowercase()) {
+        when (val normalized = offsetStr.trim().lowercase()) {
             "first" -> OffsetSpecification.first()
             "last" -> OffsetSpecification.last()
             "next" -> OffsetSpecification.next()
-            else ->
-                offsetStr.toTimestampOrNull()?.let {
-                    OffsetSpecification.timestamp(it)
-                } ?: OffsetSpecification.first()
+            else -> {
+                val timestamp =
+                    normalized.toTimestampOrNull()
+                        ?: throw IllegalArgumentException(
+                            "Invalid offset '$offsetStr'. Expected 'first', 'last', 'next' or timestamp format 'dd.MM.yyyy HH:mm:ss'.",
+                        )
+
+                OffsetSpecification.timestamp(timestamp)
+            }
         }
 
     private fun String.toTimestampOrNull(): Long? =
@@ -22,5 +30,7 @@ object RabbiOffsetResolver {
             LocalDateTime
                 .parse(this, formatter)
                 .toEpochSecond(ZoneOffset.UTC)
+        }.onFailure {
+            logger.debug("Failed to parse timestamp offset: '$this'", it)
         }.getOrNull()
 }
