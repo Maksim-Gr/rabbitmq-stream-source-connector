7fb41d882d4e178c017bad4140c7673e6172acc9
Maksim Grishchenko <grischenko1988@gmail.com>
2026-03-19 09:58:57 +0300

    deps: remove unused palantir git-version plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     kotlin("jvm") version "2.3.10"
     kotlin("plugin.serialization") version "2.3.10"
 
-    id("com.palantir.git-version") version "1.0.0"
     id("org.jlleitschuh.gradle.ktlint") version "14.1.0"
 }
 
